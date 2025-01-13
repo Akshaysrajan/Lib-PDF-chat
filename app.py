029c34ead028201ec32eaf74be0262da909400e9
Akshay S Rajan <76043412+Akshaysrajan@users.noreply.github.com>
2025-01-13 15:34:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from dotenv import load_dotenv
 load_dotenv()
 # genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 #for security will be using st.secrets
-genai.configure(api_key=st.secrets["general"]["GOOGLE_API_KEY"])
+genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
 
 
 
